166d702711c2dae10a07abff92cba77c09bae010
John Huynh <john@huynh.io>
2014-04-27 16:34:27 -0400

    Added player create event handler logic.

--- app/middleware/events/player.rb
+++ app/middleware/events/player.rb
@@ -1,8 +1,33 @@
+require File.expand_path("../../../models/player", __FILE__)
+require File.expand_path("../../../models/sprite_sheet", __FILE__)
+require File.expand_path("../../../serializers/player_serializer", __FILE__)
+
 module Events
   class Player
     class << self
-      def create
-        puts "create"
+      def create(hash)
+        name = hash["player"]["name"]
+
+        puts "Events::Player::create"
+        puts "\t request payload: #{hash}\n\n"
+
+        response_payload = {}
+
+        if name
+          base_body  = ::SpriteSheet.where(:name => "Base Body").first
+          new_player = ::Player.new(:name => name, :sprite_sheet => base_body)
+
+          if new_player.valid?
+            new_player.save
+
+            response_payload = ::PlayerSerializer.new(new_player).to_json
+          end
+        else
+          response_payload["error"] = "Events::Player::create requires a \"name\" property"
+        end
+
+        puts "\tresponse payload: #{response_payload}\n\n"
+        response_payload
       end
     end
   end
