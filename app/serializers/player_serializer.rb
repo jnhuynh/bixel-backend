b242994a40c91d2f34b42404ad36b15000416b65
John Huynh <john@huynh.io>
2014-04-27 16:25:48 -0400

    Corrected sprite sheet relation in player serializer.

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -2,5 +2,5 @@ class PlayerSerializer < ActiveModel::Serializer
   embed :ids, :include => true
 
   attributes :id, :name, :x, :y
-  has_one :spritesheet
+  has_one :sprite_sheet, :key => "sprite_sheet"
 end
