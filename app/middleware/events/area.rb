699bbc12d6c7e0af40a71dde6366103fed962a8a
John Huynh <john@huynh.io>
2014-04-29 21:43:57 -0400

    Added area index event.

--- app/middleware/events/area.rb
+++ app/middleware/events/area.rb
@@ -1,17 +1,32 @@
+require File.expand_path("../../../models/area", __FILE__)
+require File.expand_path("../../../serializers/area_serializer", __FILE__)
+
 module Events
   class Area
     class << self
-      def player_enter
+      def index
+        puts "Events::Area::index"
+        response_payload = {}
+
+        areas = ::Area.all
+        response_payload[:data] = ActiveModel::ArraySerializer
+          .new(areas, each_serializer: AreaSerializer).to_json
+
+        response_payload
+      end
+
+      def player_enter(data)
         puts "player_enter"
       end
 
-      def player_exit
+      def player_exit(data)
         puts "player_exit"
       end
 
-      def player_move
+      def player_move(data)
         puts "player_move"
       end
     end
   end
 end
+
